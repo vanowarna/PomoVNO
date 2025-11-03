98faf6821e5e4458c93f5bd6497eeb738e82fbea
VANOWARNA <vanowarna@gmail.com>
2025-11-03 17:08:37 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,13 +14,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
+    <html lang="en" className="dark">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn("font-body antialiased", "bg-background text-foreground")}>
+      <body className={cn("font-body antialiased", "bg-background text-foreground")} suppressHydrationWarning>
         {children}
         <Toaster />
       </body>
